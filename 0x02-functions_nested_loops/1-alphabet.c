bba746c75e34a1ed83891a8424bd447c9e36ef2d
kasboi <solahudeen15@gmail.com>
2022-03-15 08:02:00 -0700

    ok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ *
+ * Description: prints alphabetic order
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'A';c <= 'Z'; c++)
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
