8c4e7b625d120f5268a216e7e9f7f474111498ef
kasboi <solahudeen15@gmail.com>
2022-03-15 08:38:17 -0700

    ok

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints _putchar
+ *
+ * Description: prints alphabetic order 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c = 0;
+	while (c < 10)
+	{
+		char g;
+		for(g = 'a'; g <= 'z'; g++)
+		{
+			_putchar(g);
+		}
+		_putchar('\n');
+}
